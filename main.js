d31e40f242ef58bc48c5ded46a3cbd4ba972e83b
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-28 06:40:11 -0500

    fix: corregir
    ....

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import myHeader from "./components/myHeader.js";
 import myAside from "./components/myAside.js";
 import myBanner from "./components/myBanner.js";
@@ -24,31 +23,4 @@ mySection.listFunFacts();
 myHeader.fragShow();
 
 myFooter.listFooter();
-=======
-import myHeader from "./components/myHeader.js";
-import myAside from "./components/myAside.js";
-import myBanner from "./components/myBanner.js";
-import myPost  from "./components/myPost.js";
-import mySection from "./components/mySection.js";
-import myFooter from "./components/myFooter.js";
-
-
-myHeader.listTitle();
-
-myBanner.showImage();
-myBanner.listBanner();
-
-myPost.listDiscografia()
-myAside.showAside();
-
-mySection.listTitleSection();
-mySection.listPeriods();
-mySection.listTableDisco();
-mySection.listDiscografia();
-mySection.listTitlecuriosities();
-mySection.listFunFacts();
-myHeader.fragShow();
-
-myFooter.listFooter();
->>>>>>> f383c661d612fffd693b35fe6e55f4d227c714cf
 myFooter.listFooterContent();
--- storage/wsMyHeader.js
+++ storage/wsMyHeader.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export let wsMyHeader ={
     listAlbums(p1){
         let plantilla = "";
@@ -10,23 +9,6 @@ export let wsMyHeader ={
 }
 
 
-self.addEventListener("message", (e)=>{
+self.addEventListener("message"), (e)=>{
     postMessage(wsMyHeader[`${e.data.module}`](e.data.data));
-
-=======
-export let wsMyHeader ={
-    listAlbums(p1){
-        let plantilla = "";
-        p1.forEach((val, id) => {
-        plantilla += `<a class="p-2 link-secondary" href="${val.href}">${val.name}</a>`;
-    });
-    return plantilla;
 }
-}
-
-
-self.addEventListener("message", (e)=>{
-    postMessage(wsMyHeader[`${e.data.module}`](e.data.data));
-
->>>>>>> f383c661d612fffd693b35fe6e55f4d227c714cf
-})
